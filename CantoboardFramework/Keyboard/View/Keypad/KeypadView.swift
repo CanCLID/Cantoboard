382283d56a31df8bea2789b283901307d3631ac2
graphemecluster <graphemecluster@gmail.com>
2025-04-01 15:31:12 +0800

    Fix incorrect preview (popup) display condition If there aren't multiple choices, popup should not be shown even during long press if the settings item is disabled.
    (cherry picked from commit 9d3b1a85126272c8cf37d0309a1a7da49afbaa76)

--- CantoboardFramework/Keyboard/View/Keypad/KeypadView.swift
+++ CantoboardFramework/Keyboard/View/Keypad/KeypadView.swift
@@ -147,6 +147,7 @@ class KeypadView: UIView, BaseKeyboardView {
                 } else {
                     button = KeypadButton(layoutConstants: layoutConstants)
                     button.titleLabel?.adjustsFontSizeToFitWidth = true
+                    button.shouldDisablePreview = true
                     addSubview(button)
                 }
                 
